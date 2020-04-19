d72445559eb26572d772c56503099cc75032ec1f
ggbeppe <ggbeppe@users.noreply.github.com>
2020-04-19 15:49:02 +0200

    Added basic mvc structure

---
+++ mvc/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ggbeppe/golang-microservices/mvc/app"
+)
+
+func main() {
+	app.StartApp()
+}
---
+++ mvc/services/users_service.go
@@ -0,0 +1,9 @@
+package services
+
+import ("github.com/ggbeppe/golang-microservices/mvc/domain"
+	"github.com/ggbeppe/golang-microservices/mvc/utils"
+)
+
+func GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
+	return domain.GetUser(userId)
+}
